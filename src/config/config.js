22d4cb3abdfbae265d53586b8eff0aee727dbbe3
Farzan <farzan.hosseiny@gmail.com>
2023-07-17 20:11:25 +0330

    deploying on vercel

---
+++ src/config/config.js
@@ -0,0 +1,10 @@
+const config = {
+  development: {
+    backendUrl: "https://project-threejs-ai-ya55.onrender.com/api/v1/dalle",
+  },
+  production: {
+    backendUrl: "https://devswag.onrender.com/api/v1/dalle",
+  },
+};
+
+export default config;
